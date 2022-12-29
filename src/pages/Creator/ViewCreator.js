667fe1ca5e4dc6c411910f610c21e7d05668a417
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2022-12-30 00:41:01 +0530

    code image related

--- src/pages/Creator/ViewCreator.js
+++ src/pages/Creator/ViewCreator.js
@@ -156,22 +156,22 @@ const ViewCreator = () => {
           <Grid item xs={6} className="veiw-card">
             <p className="label">Aadhar Card</p>
             <figure className='view-doc d-flex-start-start'>
-              <img src={toAbsoluteUrl("/images/view-doc.png")} alt="" />
-              <img src={toAbsoluteUrl("/images/view-doc.png")} alt="" />
+              <img src={creatorDetail?.adhar_front_url ? creatorDetail.adhar_front_url : toAbsoluteUrl("/images/view-doc.png")} alt="" style={{maxWidth: creatorDetail?.adhar_front_url ? '30%' : ''}} />
+              <img src={creatorDetail?.adhar_back_url ? creatorDetail.adhar_back_url : toAbsoluteUrl("/images/view-doc.png")} alt="" style={{maxWidth: creatorDetail?.adhar_front_url ? '30%' : ''}} />
             </figure>
             <Chip icon={<KycStatus svgFill='#1B5E20' />} label={`${(creatorDetail?.is_adhar_verified == 1) || (adharCardflag == 1) ? 'Verified' : 'Not Verified'}`} variant="outlined" className={`${creatorDetail?.is_adhar_verified == 1 ? 'verified-tag filled' : 'verified-tag'}`} onClick={() => handleClick(creatorDetail?.is_adhar_verified, "adhar_verification")} />
           </Grid>
           <Grid item xs={6} className="veiw-card">
             <p className="label">PAN Card</p>
             <figure className='view-doc d-flex-start-start'>
-              <img src={toAbsoluteUrl("/images/view-doc.png")} alt="" />
+              <img src={creatorDetail?.pan_card_url ? creatorDetail.pan_card_url : toAbsoluteUrl("/images/view-doc.png")} alt="" style={{maxWidth: creatorDetail?.pan_card_url ? '30%' : ''}} />
             </figure>
             <Chip icon={<KycStatus svgFill='#1B5E20' />} label={`${(creatorDetail?.is_pan_card_verified == 1) || (panCardflag == 1) ? 'Verified' : 'Not Verified'}`} variant="outlined" className={`${(creatorDetail?.is_pan_card_verified == 1) || (panCardflag == 1) ? 'verified-tag filled' : 'verified-tag'}`} onClick={() => handleClick(creatorDetail?.is_pan_card_verified, "pan_card_verification")} />
           </Grid>
           <Grid item xs={6} className="veiw-card">
             <p className="label">Passbook</p>
             <figure className='view-doc d-flex-start-start'>
-              <img src={toAbsoluteUrl("/images/view-doc.png")} alt="" />
+              <img src={creatorDetail?.passbook_url ? creatorDetail.passbook_url : toAbsoluteUrl("/images/view-doc.png")} alt="" style={{maxWidth: creatorDetail?.passbook_url ? '30%' : ''}} />
             </figure>
             <Chip icon={<KycStatus svgFill='#1B5E20' />} label={`${(creatorDetail?.is_passbook_verified == 1) || (pashbookflag == 1) ? 'Verified' : 'Not Verified'}`} variant="outlined" className={`${creatorDetail?.is_passbook_verified == 1 ? 'verified-tag filled' : 'verified-tag'}`} onClick={() => handleClick(creatorDetail?.is_passbook_verified, "passbook_verification")} />
           </Grid>
