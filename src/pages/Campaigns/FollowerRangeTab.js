35080e6b9d3721d18ecc386c8ef62a75ae226d31
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-01-11 22:55:35 +0530

    code error remove

--- src/pages/Campaigns/FollowerRangeTab.js
+++ src/pages/Campaigns/FollowerRangeTab.js
@@ -1,10 +1,85 @@
 import { Autocomplete, Button, FormControl, Grid, InputAdornment, InputLabel, MenuItem, Select, TextField } from '@mui/material'
 import { Box, Stack } from '@mui/system'
 import { DataGrid } from '@mui/x-data-grid'
-import React from 'react'
+import React, { useState } from 'react'
 import { SearchIcon } from '../../svg'
 
+const columns = [
+    {
+      field: "id",
+      headerName: "Sr No.",
+      minWidth: 60,
+    },
+    {
+      field: "creator_name",
+      headerName: "Creator Name",
+      minWidth: 220,
+      sortable: false,
+      filterable: false,
+    },
+    {
+      field: "followers",
+      headerName: "Followers",
+      minWidth: 120,
+    },
+    {
+      field: "state",
+      headerName: "State",
+      minWidth: 180,
+    },
+    {
+      field: "contact",
+      headerName: "Contact",
+      minWidth: 180,
+    },
+    {
+      field: "category",
+      headerName: "Category",
+      minWidth: 110,
+    },
+  ];
+  
+  const rows = [
+    { id: 1, creator_name: 'Johny Depp', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 2, creator_name: 'Cersei', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 3, creator_name: 'Jaime', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 4, creator_name: 'Arya', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 5, creator_name: 'Daenerys', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 6, creator_name: 'Bhautik', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 7, creator_name: 'Ferrara', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 8, creator_name: 'Rossini', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 9, creator_name: 'Harvey', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 10, creator_name: 'Daenerys', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 11, creator_name: 'Rahul', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 12, creator_name: 'Ferrara', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 13, creator_name: 'Rossini', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+    { id: 14, creator_name: 'Harvey', followers: '3.5M', state: 'Maharashtra', contact: '+91 0000000000', category: 'Beauty' },
+  ];
+
 function FollowerRangeTab() {
+
+    const [kycList, setKycList] = useState([]);
+  const [search, setSearched] = useState("");
+  const [filter, /* setFilter */] = useState()
+
+  const onMutate = (e, value) => {
+    setSearched(value);
+  };
+
+  const handleChangeFilter = (e) => {
+    // if (e.target.value == '5') {
+    //   getAllKycListing();
+    // } else {
+    //   dispatch(KycFilterListing(`?kyc_filter=${e.target.value}`))
+    //     .then((res) => {
+    //       setKycList(res.data);
+    //     })
+    //     .catch((err) => {
+    //       toast.error(err);
+    //     });
+    // }
+  };
+
     const top100Films = [
         { title: 'The Shawshank Redemption', year: 1994 },
         { title: 'The Godfather', year: 1972 },
