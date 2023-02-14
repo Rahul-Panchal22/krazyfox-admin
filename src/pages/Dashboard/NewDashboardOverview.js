c1d8bf2af1673bc970c74536433fdb26c46e7a7d
Rahul Panchal <rahulpydev22@gmail.com>
2023-02-14 16:22:48 +0530

    completed heat map layer in dashboard

--- src/pages/Dashboard/NewDashboardOverview.js
+++ src/pages/Dashboard/NewDashboardOverview.js
@@ -18,49 +18,78 @@ import CreatorsGenre from "./CreatorsGenre";
 import { useDispatch } from "react-redux";
 import { DashboardData } from "../../actions/dashboard";
 import { useNavigate } from "react-router-dom";
-import { GoogleMap , HeatmapLayer, LoadScript, useLoadScript } from '@react-google-maps/api';
+import {
+  GoogleMap,
+  HeatmapLayer,
+  LoadScript,
+  useJsApiLoader,
+  useLoadScript,
+} from "@react-google-maps/api";
+import GoogleMapReact from "google-map-react";
 
 const containerStyle = {
-  width: '100%',
-    height: '100%'
-  };
-  
-  // const placesLibrary = ['places','visualization']
-  
-const NewDashboardOverview = () => {
+  width: "100%",
+  height: "100%",
+};
 
+const placesLibrary = ["places"];
+
+// const placesLibrary = ['places','visualization']
+
+const NewDashboardOverview = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [value, setValue] = React.useState("1");
-  const [map, setMap] = React.useState(null)
-  const [searchResult, setSearchResult] = useState('');
+  const [map, setMap] = React.useState(null);
+  const [searchResult, setSearchResult] = useState("");
   const [activeMarker, setActiveMarker] = useState(null);
-  const [dashboardCountData , setDashBoardCountData] = useState(null);
-  // const [heatmapData, setHeatmapData] = useState(dashboardCountData?.getUserLatLong);
-  
-  useEffect(()=> {
+  const [dashboardCountData, setDashBoardCountData] = useState(null);
+  const [heatmapData, setHeatmapData] = useState([]);
+  const [center, setCenter] = useState({
+    lat: 23.0225,
+    lng: 72.5714,
+  });
+  useEffect(() => {
     dispatch(DashboardData())
-    .then(res =>
-      setDashBoardCountData(res.data))
-    .catch( err => console.log(err))
-  },[])
+      .then((res) => {
+        setDashBoardCountData(res.data);
+        setHeatmapData(res.data.getUserLatLong);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyB3qfOgTiJdEa5hv_l03saH8RMne_sQzqM",
+    libraries: ["visualization"],
+  });
+
+  const heatmapDatas = {
+    positions: heatmapData,
+    options: {
+      radius: 20,
+      opacity: 1,
+    },
+  };
+
   return (
     <>
-      <Grid container spacing={2} className='mar-bottom-40'>
+      <Grid container spacing={2} className="mar-bottom-40">
         <Grid item xs={6}>
           <h6 className="overview-heading">User</h6>
-          <div className="border-paper padd-24 h-100--56 d-block">
+          <div className="border-paper padd-24 h-100--56 d-block" onClick={() => navigate('/creator')}>
             <OverviewCard
               cardHeading={dashboardCountData?.totalUser}
               averageView="Today's Active Users"
               cardIcon="active-user-icon.svg"
             />
             <h6 className="chart-heading mar-top-20">Daily Active Users</h6>
-            <p className="chart-digit pink">{dashboardCountData?.totalUser} Users</p>
+            <p className="chart-digit pink">
+              {dashboardCountData?.totalUser} Users
+            </p>
             <DailyActiveUsers />
           </div>
         </Grid>
@@ -69,7 +98,10 @@ const NewDashboardOverview = () => {
           <div className="border-paper padd-24 h-100--56 d-block">
             <Box className="stack-button-tab">
               <TabContext value={value}>
-                <TabList onChange={handleChange} aria-label="lab API tabs example">
+                <TabList
+                  onChange={handleChange}
+                  aria-label="lab API tabs example"
+                >
                   <Tab label="All" value="1" />
                   <Tab label="HyperLocal" value="2" />
                   <Tab label="Campaign" value="3" />
@@ -78,46 +110,62 @@ const NewDashboardOverview = () => {
                   <div className="d-flex-center-center column-gap-16">
                     <CampaignsCard
                       campaignTitle="Completed Campaigns"
-                      completedCampaign={dashboardCountData?.pendingCampaignCount}
+                      completedCampaign={
+                        dashboardCountData?.pendingCampaignCount
+                      }
                       totalCampaign={dashboardCountData?.overAllCampaignCount}
                       completedToday={0}
                       completedCard={true}
                       cardColor="green"
-                      navigationPath='/creator'
+                      navigationPath="/campaigns"
                     />
                     <CampaignsCard
                       campaignTitle="Pending Campaigns"
-                      completedCampaign={dashboardCountData?.completedCampaignCount}
+                      completedCampaign={
+                        dashboardCountData?.completedCampaignCount
+                      }
                       // totalCampaign={160}
                       completedToday={0}
                       completedCard={true}
                       cardColor="orange"
+                      navigationPath="/campaigns"
                     />
                   </div>
-                  <h6 className="chart-heading mar-top-20">Campaigns Applications</h6>
+                  <h6 className="chart-heading mar-top-20">
+                    Campaigns Applications
+                  </h6>
                   <p className="chart-digit sky">580 Completed</p>
                   <DailyActiveUsers />
                 </TabPanel>
                 <TabPanel value="2">
                   <div className="d-flex-center-center column-gap-16">
                     <CampaignsCard
-                      campaignTitle="Completed Campaigns"
-                      completedCampaign={dashboardCountData?.completedHyperLocalCampaignCount}
-                      totalCampaign={dashboardCountData?.overAllHyperLocalCampaignCount}
+                      campaignTitle="Completed Hyper Local Campaigns"
+                      completedCampaign={
+                        dashboardCountData?.completedHyperLocalCampaignCount
+                      }
+                      totalCampaign={
+                        dashboardCountData?.overAllHyperLocalCampaignCount
+                      }
                       completedToday={0}
                       completedCard={true}
                       cardColor="green"
-                      navigationPath='/hyperlocal'
+                      navigationPath="/hyperlocal"
                     />
                     <CampaignsCard
-                      campaignTitle="Pending Campaigns"
-                      completedCampaign={dashboardCountData?.pendingHyperLocalCampaignCount}
+                      campaignTitle="Pending Hyper Local Campaigns"
+                      completedCampaign={
+                        dashboardCountData?.pendingHyperLocalCampaignCount
+                      }
                       completedToday={0}
                       completedCard={true}
                       cardColor="orange"
+                      navigationPath="/hyperlocal"
                     />
                   </div>
-                  <h6 className="chart-heading mar-top-20">Campaigns Applications</h6>
+                  <h6 className="chart-heading mar-top-20">
+                    Campaigns Applications
+                  </h6>
                   <p className="chart-digit sky">580 Completed</p>
                   <DailyActiveUsers />
                 </TabPanel>
@@ -125,22 +173,31 @@ const NewDashboardOverview = () => {
                   <div className="d-flex-center-center column-gap-16">
                     <CampaignsCard
                       campaignTitle="Completed Campaigns"
-                      completedCampaign={dashboardCountData?.completedAllCampaignCountWithOut}
-                      totalCampaign={dashboardCountData?.overAllCampaignCountWithOut}
+                      completedCampaign={
+                        dashboardCountData?.completedAllCampaignCountWithOut
+                      }
+                      totalCampaign={
+                        dashboardCountData?.overAllCampaignCountWithOut
+                      }
                       completedToday={0}
                       completedCard={true}
                       cardColor="green"
-                      navigationPath='/campaigns'
+                      navigationPath="/campaigns"
                     />
                     <CampaignsCard
                       campaignTitle="Pending Campaigns"
-                      completedCampaign={dashboardCountData?.pendingCampaignCountWithOut}
+                      completedCampaign={
+                        dashboardCountData?.pendingCampaignCountWithOut
+                      }
                       completedToday={0}
                       completedCard={true}
                       cardColor="orange"
+                      navigationPath="/campaigns"
                     />
                   </div>
-                  <h6 className="chart-heading mar-top-20">Campaigns Applications</h6>
+                  <h6 className="chart-heading mar-top-20">
+                    Campaigns Applications
+                  </h6>
                   <p className="chart-digit sky">580 Completed</p>
                   <DailyActiveUsers />
                 </TabPanel>
@@ -165,34 +222,37 @@ const NewDashboardOverview = () => {
           </Grid>
           <Grid container item xs={7} className="h-100">
             <Grid item xs={6} className="h-100">
-              <CreatorsAge creatorsAgeData={dashboardCountData?.ageCount}/>
+              <CreatorsAge creatorsAgeData={dashboardCountData?.ageCount} />
             </Grid>
             <Grid item xs={6} className="h-100">
-              <CreatorsGender creatorsGender={dashboardCountData?.genderCount} />
+              <CreatorsGender
+                creatorsGender={dashboardCountData?.genderCount}
+              />
             </Grid>
             <Grid item xs={6} className="h-100">
-              <CreatorsState creatorsState={dashboardCountData?.stateCount}/>
+              <CreatorsState creatorsState={dashboardCountData?.stateCount} />
             </Grid>
             <Grid item xs={6} className="h-100">
-              <CreatorsGenre creatorsGenre={dashboardCountData?.genreCount}/>
+              <CreatorsGenre creatorsGenre={dashboardCountData?.genreCount} />
             </Grid>
           </Grid>
           <Grid item xs={12} className="h-100">
             <h6 className="chart-heading">Creator Locations</h6>
             <div className="map-section">
-            <LoadScript
-  googleMapsApiKey={process.env.GOOGLEMAP_API_KEY}
-  libraries={['visualization']}
->
-<GoogleMap
-  mapContainerStyle={{ height: "400px", width: "100%" }}
-  zoom={13}
-  center={{ lat: 37.782551, lng: -122.445368 }}
->
-  {/* <HeatmapLayer data={heatmapData} /> */}
-</GoogleMap>
-
-</LoadScript>
+              {isLoaded ? (
+                <GoogleMapReact
+                  bootstrapURLKeys={{
+                    key: "AIzaSyB3qfOgTiJdEa5hv_l03saH8RMne_sQzqM",
+                  }}
+                  defaultZoom={11}
+                  center={center}
+                  heatmapLibrary={true}
+                  heatmap={heatmapDatas}
+                >
+                </GoogleMapReact>
+              ) : (
+                <></>
+              )}
             </div>
           </Grid>
         </Grid>
